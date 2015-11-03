1afd3ef6098cb34212ebaf5702f2ce905b6d1f11
Tommaso Soru <mommi84@gmail.com>
2015-11-03 16:51:59 +0100

    evaluation package structure

---
+++ limes-core/src/main/java/org/aksw/limes/core/evaluation/AUC.java
@@ -0,0 +1,21 @@
+package org.aksw.limes.core.evaluation;
+
+import java.util.Set;
+
+import org.aksw.limes.core.model.Link;
+
+/**
+ * @author Mofeed Hassan <mounir@informatik.uni-leipzig.de>
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ * @version 2015-11-03
+ *
+ */
+public class AUC implements QualitativeMeasure {
+
+	@Override
+	public double calculate(Set<Link> predictions, Set<Link> goldStandard) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ limes-core/src/main/java/org/aksw/limes/core/evaluation/FMeasure.java
@@ -0,0 +1,27 @@
+package org.aksw.limes.core.evaluation;
+
+import java.util.Set;
+
+import org.aksw.limes.core.model.Link;
+
+/**
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ * @version 2015-11-03
+ *
+ */
+public class FMeasure implements QualitativeMeasure {
+
+	@Override
+	public double calculate(Set<Link> predictions, Set<Link> goldStandard) {
+		
+		double p = new Precision().calculate(predictions, goldStandard);
+		double r = new Recall().calculate(predictions, goldStandard);
+		
+		if(p + r > 0d)
+			return 2 * p * r / (p + r);
+		else
+			return 0d;
+		
+	}
+
+}
