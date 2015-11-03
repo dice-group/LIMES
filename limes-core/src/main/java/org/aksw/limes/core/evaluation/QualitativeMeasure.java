7a7dcecdefe08d213d6a6d040dbc083c1c1d549b
Tommaso Soru <mommi84@gmail.com>
2015-11-03 15:29:26 +0100

    qualitative measure interface, added Jena dependencies

--- limes-core/src/main/java/org/aksw/limes/core/evaluation/QualitativeMeasure.java
+++ limes-core/src/main/java/org/aksw/limes/core/evaluation/QualitativeMeasure.java
@@ -1,9 +1,17 @@
 package org.aksw.limes.core.evaluation;
 
+import java.util.Set;
+
+import org.aksw.limes.core.model.Link;
+
 /**
+ * @author Mofeed Hassan <mounir@informatik.uni-leipzig.de>
  * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ * @version 2015-11-03
  *
  */
-public interface ExampleInterface {
+public interface QualitativeMeasure {
+	
+	public double calculate(Set<Link> predictions, Set<Link> goldStandard);
 
 }
---
+++ limes-core/src/main/java/org/aksw/limes/core/model/Link.java
@@ -0,0 +1,31 @@
+package org.aksw.limes.core.model;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+
+/**
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ * @version 2015-11-03
+ * 
+ */
+public class Link extends Triple implements Comparable<Link> {
+	
+	private String hash;
+
+	public Link(Node s, Node p, Node o) {
+		super(s, p, o);
+		this.hash = DigestUtils.shaHex(s.getURI() + "" + p.getURI() + "" + o.toString());
+	}
+
+	@Override
+	public int compareTo(Link o) {
+		return this.hash.compareTo(o.getHash());
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+}
