ebdcabafdf7598bc414b94a9f18fb31c2f414915
Kleanthi Georgala <kleanthi@ainote6.informatik.uni-leipzig.de>
2015-11-05 16:21:27 +0100

    Adding tests

---
+++ limes-core/src/test/java/measures/mapper/SetOperationsTest.java
@@ -0,0 +1,21 @@
+package measures.mapper;
+
+import org.aksw.limes.core.io.mapping.Mapping;
+import org.aksw.limes.core.io.mapping.MemoryMapping;
+import org.aksw.limes.core.measures.mapper.SetOperations;
+
+public class SetOperationsTest {
+    public static void main(String args[]) {
+	Mapping a = new MemoryMapping();
+	Mapping b = new MemoryMapping();
+	SetOperations opers = new SetOperations();
+	a.add("c", "c", 0.5);
+	a.add("a", "z", 0.5);
+	a.add("a", "b", 0.5);
+	b.add("a", "c", 0.5);
+	b.add("a", "b", 0.7);
+	b.add("b", "y", 0.7);
+	System.out.println(opers.union(a, b));
+    }
+
+}
