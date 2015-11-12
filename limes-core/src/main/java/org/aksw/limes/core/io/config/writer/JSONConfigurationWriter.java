1a2e448c5690e2a8623548c04e61cb623aa4f84d
MSherif <mohameddotsherif@gmail.com>
2015-11-12 16:26:37 +0100

    remove json reader and writer, add json-ld to RDFConfig

--- limes-core/src/main/java/org/aksw/limes/core/io/config/writer/JSONConfigurationWriter.java
+++
@@ -1,5 +0,0 @@
-package org.aksw.limes.core.io.config.writer;
-
-public class JSONConfigurationWriter implements IConfigurationWriter{
-
-}
--- limes-core/src/main/java/org/aksw/limes/core/io/config/writer/XMLConfigurationWriter.java
+++ limes-core/src/main/java/org/aksw/limes/core/io/config/writer/XMLConfigurationWriter.java
@@ -1,5 +1,23 @@
 package org.aksw.limes.core.io.config.writer;
 
+import java.io.IOException;
+
+import org.aksw.limes.core.io.config.Configuration;
+
 public class XMLConfigurationWriter implements IConfigurationWriter{
+
+	@Override
+	public void write(Configuration configuration, String outputFile,
+			String format) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void write(Configuration configuration, String outputFile)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
