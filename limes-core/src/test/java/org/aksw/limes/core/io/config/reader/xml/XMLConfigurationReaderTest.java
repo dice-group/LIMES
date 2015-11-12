9b134c23469277e7f60ab3de924055575cff5782
MSherif <mohameddotsherif@gmail.com>
2015-11-12 18:06:03 +0100

    test xml reader

---
+++ limes-core/src/test/java/org/aksw/limes/core/io/config/reader/xml/XMLConfigurationReaderTest.java
@@ -0,0 +1,16 @@
+package org.aksw.limes.core.io.config.reader.xml;
+
+
+public class XMLConfigurationReaderTest {
+	
+	static void testRead(){
+		XMLConfigurationReader c = new XMLConfigurationReader();
+		System.out.println(c.read("/resources/lgd-lgd.xml"));
+	}
+	
+	public static void main(String[] args){
+		testRead();
+		
+	}
+
+}
