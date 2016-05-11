b9ee1c3f66e7e3db66a0dff908089ecb58d213ce
MSherif <mohameddotsherif@gmail.com>
2016-05-11 11:24:29 +0200

    refactorand comenting NaiveMin

--- limes-core/src/main/java/org/aksw/limes/core/measures/measure/pointsets/min/NaiveMin.java
+++ limes-core/src/main/java/org/aksw/limes/core/measures/measure/pointsets/min/NaiveMin.java
@@ -8,8 +8,6 @@ import org.aksw.limes.core.io.cache.Instance;
 import org.aksw.limes.core.io.mapping.Mapping;
 import org.aksw.limes.core.io.mapping.MemoryMapping;
 import org.aksw.limes.core.measures.mapper.atomic.OrchidMapper;
-import org.aksw.limes.core.measures.mapper.atomic.hausdorff.GreatEllipticDistance;
-import org.aksw.limes.core.measures.mapper.atomic.hausdorff.OrthodromicDistance;
 import org.aksw.limes.core.measures.mapper.atomic.hausdorff.Polygon;
 import org.aksw.limes.core.measures.measure.pointsets.PointsetsMeasure;
 
@@ -23,134 +21,133 @@ import java.util.TreeSet;
  */
 public class NaiveMin extends PointsetsMeasure {
 
-    public int computations;
+	/**
+	 * Brute force approach to computing the MAX distance between two polygons
+	 *
+	 * @param X First polygon
+	 * @param Y Second polygon
+	 * @return Distance between the two polygons
+	 */
+	public NaiveMin() {
+		computations = 0;
+	}
 
-    /**
-     * Brute force approach to computing the MAX distance between two polygons
-     *
-     * @param X
-     *            First polygon
-     * @param Y
-     *            Second polygon
-     * @return Distance between the two polygons
-     */
-    public NaiveMin() {
-	computations = 0;
-    }
+	public int getComputations() {
+		return computations;
+	}
 
-    public int getComputations() {
-	return computations;
-    }
-
-    public double computeDistance(Polygon X, Polygon Y, double threshold) {
-	double min = Double.MAX_VALUE;
-	double d;
-	for (Point x : X.points) {
-	    for (Point y : Y.points) {
-		d = distance(x, y);
-		if (min > d) {
-		    min = d;
+	/* (non-Javadoc)
+	 * @see org.aksw.limes.core.measures.measure.pointsets.IPointsetsMeasure#computeDistance(org.aksw.limes.core.measures.mapper.atomic.hausdorff.Polygon, org.aksw.limes.core.measures.mapper.atomic.hausdorff.Polygon, double)
+	 */
+	public double computeDistance(Polygon X, Polygon Y, double threshold) {
+		double min = Double.MAX_VALUE;
+		double d;
+		for (Point x : X.points) {
+			for (Point y : Y.points) {
+				d = pointToPointDistance(x, y);
+				if (min > d) {
+					min = d;
+				}
+			}
 		}
-	    }
+		return min;
 	}
-	return min;
-    }
 
-    public static double distance(Polygon X, Polygon Y, double threshold) {
-	double min = Double.MAX_VALUE;
-	double d;
-	for (Point x : X.points) {
-	    for (Point y : Y.points) {
-		d = (new NaiveMin()).distance(x, y);
-		if (min < d) {
-		    min = d;
+	/**
+	 * @param X First polygon
+	 * @param Y Second polygon
+	 * @param threshold
+	 * @return
+	 */
+	public static double distance(Polygon X, Polygon Y, double threshold) {
+		double min = Double.MAX_VALUE;
+		double d;
+		for (Point x : X.points) {
+			for (Point y : Y.points) {
+				d = pointToPointDistance(x, y);
+				if (min < d) {
+					min = d;
+				}
+			}
+
 		}
-	    }
-
+		return min;
 	}
-	return min;
-    }
 
-    public String getName() {
-	return "naiveMin";
-    }
+	public String getName() {
+		return "naiveMin";
+	}
 
-    /**
-     * Computes the SetMeasure distance for a source and target set
-     *
-     * @param source
-     *            Source polygons
-     * @param target
-     *            Target polygons
-     * @param threshold
-     *            Distance threshold
-     * @return Mapping of uris
-     */
-    public Mapping run(Set<Polygon> source, Set<Polygon> target, double threshold) {
-	Mapping m = new MemoryMapping();
-	for (Polygon s : source) {
-	    for (Polygon t : target) {
-		double d = computeDistance(s, t, threshold);
-		if (d <= threshold) {
-		    m.add(s.uri, t.uri, d);
+	/**
+	 * Computes the SetMeasure distance for a source and target set
+	 *
+	 * @param source Source polygons
+	 * @param target Target polygons
+	 * @param threshold Distance threshold
+	 * @return Mapping of resources from Source to Target
+	 */
+	public Mapping run(Set<Polygon> source, Set<Polygon> target, double threshold) {
+		Mapping m = new MemoryMapping();
+		for (Polygon s : source) {
+			for (Polygon t : target) {
+				double d = computeDistance(s, t, threshold);
+				if (d <= threshold) {
+					m.add(s.uri, t.uri, d);
+				}
+			}
 		}
-	    }
+		return m;
 	}
-	return m;
-    }
 
-    /**
-     * @param x
-     *            Point x
-     * @param y
-     *            Point y
-     * @return Distance between x and y
-     */
-    public double distance(Point x, Point y) {
-	computations++;
-	if (USE_GREAT_ELLIPTIC_DISTANCE) {
-	    return GreatEllipticDistance.getDistanceInDegrees(x, y);
+		/* (non-Javadoc)
+		 * @see org.aksw.limes.core.measures.measure.IMeasure#getSimilarity(java.lang.Object, java.lang.Object)
+		 */
+		public double getSimilarity(Object a, Object b) {
+		Polygon p1 = OrchidMapper.getPolygon((String) a);
+		Polygon p2 = OrchidMapper.getPolygon((String) b);
+		double d = computeDistance(p1, p2, 0);
+		return 1d / (1d + (double) d);
 	}
-	return OrthodromicDistance.getDistanceInDegrees(x, y);
-    }
 
-    public double getSimilarity(Object a, Object b) {
-	Polygon p1 = OrchidMapper.getPolygon((String) a);
-	Polygon p2 = OrchidMapper.getPolygon((String) b);
-	double d = computeDistance(p1, p2, 0);
-	return 1d / (1d + (double) d);
-    }
-
-    public String getType() {
-	return "geodistance";
-    }
-
-    public double getSimilarity(Instance a, Instance b, String property1, String property2) {
-	TreeSet<String> source = a.getProperty(property1);
-	TreeSet<String> target = b.getProperty(property2);
-	Set<Polygon> sourcePolygons = new HashSet<Polygon>();
-	Set<Polygon> targetPolygons = new HashSet<Polygon>();
-	for (String s : source) {
-	    sourcePolygons.add(OrchidMapper.getPolygon(s));
+	/* (non-Javadoc)
+	 * @see org.aksw.limes.core.measures.measure.IMeasure#getType()
+	 */
+	public String getType() {
+		return "geodistance";
 	}
-	for (String t : target) {
-	    targetPolygons.add(OrchidMapper.getPolygon(t));
-	}
-	double min = Double.MAX_VALUE;
-	double d = 0;
-	for (Polygon p1 : sourcePolygons) {
-	    for (Polygon p2 : targetPolygons) {
-		d = computeDistance(p1, p2, 0);
-		if (d < min) {
-		    min = d;
+
+	/* (non-Javadoc)
+	 * @see org.aksw.limes.core.measures.measure.IMeasure#getSimilarity(org.aksw.limes.core.io.cache.Instance, org.aksw.limes.core.io.cache.Instance, java.lang.String, java.lang.String)
+	 */
+	public double getSimilarity(Instance a, Instance b, String property1, String property2) {
+		TreeSet<String> source = a.getProperty(property1);
+		TreeSet<String> target = b.getProperty(property2);
+		Set<Polygon> sourcePolygons = new HashSet<Polygon>();
+		Set<Polygon> targetPolygons = new HashSet<Polygon>();
+		for (String s : source) {
+			sourcePolygons.add(OrchidMapper.getPolygon(s));
 		}
-	    }
+		for (String t : target) {
+			targetPolygons.add(OrchidMapper.getPolygon(t));
+		}
+		double min = Double.MAX_VALUE;
+		double d = 0;
+		for (Polygon p1 : sourcePolygons) {
+			for (Polygon p2 : targetPolygons) {
+				d = computeDistance(p1, p2, 0);
+				if (d < min) {
+					min = d;
+				}
+			}
+		}
+		return 1d / (1d + (double) d);
 	}
-	return 1d / (1d + (double) d);
-    }
 
-    public double getRuntimeApproximation(double mappingSize) {
-	return mappingSize / 1000d;
-    }
+	/* (non-Javadoc)
+	 * @see org.aksw.limes.core.measures.measure.IMeasure#getRuntimeApproximation(double)
+	 */
+	public double getRuntimeApproximation(double mappingSize) {
+		return mappingSize / 1000d;
+	}
 
 }
