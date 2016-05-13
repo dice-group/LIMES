e5fff06410275dc423b629efae2f7b2fdd4586c1
Tommaso Soru <mommi84@gmail.com>
2016-05-13 13:34:15 +0200

    added basic Javadoc to ML interfaces

--- limes-core/src/main/java/org/aksw/limes/core/ml/algorithm/IMLAlgorithm.java
+++ limes-core/src/main/java/org/aksw/limes/core/ml/algorithm/IMLAlgorithm.java
@@ -9,14 +9,43 @@ import org.aksw.limes.core.ml.setting.LearningSetting;
  */
 public interface IMLAlgorithm {
 
+	/**
+	 * Name of the algorithm.
+	 * 
+	 * @return the name of the algorithm
+	 */
 	public String getName();
-
+	
+	/**
+	 * Generate the machine-learning model and the associated link specification using the given mapping as training data.
+	 * 
+	 * @param trainingData mapping as training data
+	 * @return an object containing the learned link specification and details about the training task
+	 */
 	public MLResult learn(Mapping trainingData);
 	
+	/**
+	 * Compute the mapping, i.e. all predicted links among source and target datasets.
+	 * 
+	 * @return the mapping
+	 */
 	public Mapping computePredictions();
 
+	/**
+	 * Initiate the machine-learning algorithm.
+	 * 
+	 * @param parameters algorithm-specific hyperparameters
+	 * @param trainingData all the training data (i.e., the whole mapping with or without labels)
+	 * @throws Exception
+	 */
 	void init(LearningSetting parameters, Mapping trainingData) throws Exception;
 	
+	/**
+	 * XXX Status: TBD
+	 * 
+	 * Terminate the algorithm.
+	 * 
+	 */
 	void terminate();
 	
 }
--- limes-core/src/main/java/org/aksw/limes/core/ml/algorithm/MLAlgorithm.java
+++ limes-core/src/main/java/org/aksw/limes/core/ml/algorithm/MLAlgorithm.java
@@ -17,45 +17,57 @@ public abstract class MLAlgorithm implements IMLAlgorithm {
 	protected Cache sourceCache;
 	protected Cache targetCache;
 	static Logger logger = Logger.getLogger("LIMES");
-//	protected Mapping mapping;
 
-	public MLAlgorithm(Cache sourceCache, Cache targetCache, Configuration configuration) {//, Mapping mapping) {
+	/**
+	 * @param sourceCache
+	 * @param targetCache
+	 * @param configuration
+	 */
+	protected MLAlgorithm(Cache sourceCache, Cache targetCache, Configuration configuration) {//, Mapping mapping) {
 		super();
 		this.setSourceCache(sourceCache);
 		this.setTargetCache(targetCache);
 		this.setConfiguration(configuration);
-//		this.mapping = mapping;
 	}
-//	
-//	private void setOutStreams(String name) throws FileNotFoundException {
-//		File stdFile = new File(name+"_stdOut.txt");
-//		PrintStream stdOut = new PrintStream(new FileOutputStream(stdFile, false));
-//		File errFile = new File(name+"_errOut.txt");
-//		PrintStream errOut = new PrintStream(new FileOutputStream(errFile, false));
-//		System.setErr(errOut);
-//		System.setOut(stdOut);
-//	}
 
+	/**
+	 * @return the LIMES configuration
+	 */
 	public Configuration getConfiguration() {
 		return configuration;
 	}
 
+	/**
+	 * @param configuration the LIMES configuration
+	 */
 	public void setConfiguration(Configuration configuration) {
 		this.configuration = configuration;
 	}
 
+	/**
+	 * @return the source dataset cache
+	 */
 	public Cache getSourceCache() {
 		return sourceCache;
 	}
 
+	/**
+	 * @param sourceCache the source dataset cache
+	 */
 	public void setSourceCache(Cache sourceCache) {
 		this.sourceCache = sourceCache;
 	}
 
+	/**
+	 * @return the target dataset cache
+	 */
 	public Cache getTargetCache() {
 		return targetCache;
 	}
 
+	/**
+	 * @param targetCache the target dataset cache
+	 */
 	public void setTargetCache(Cache targetCache) {
 		this.targetCache = targetCache;
 	}
