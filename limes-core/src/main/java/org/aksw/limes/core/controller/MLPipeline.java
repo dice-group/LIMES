49e541ad8e3eec69f68d56d3f660820c21bef4f1
Kevin Dreßler <kvndrsslr@gmail.com>
2016-05-26 12:28:42 +0200

    - Cleaning up code base

--- limes-core/src/main/java/org/aksw/limes/core/controller/MLPipeline.java
+++ limes-core/src/main/java/org/aksw/limes/core/controller/MLPipeline.java
@@ -3,38 +3,64 @@ package org.aksw.limes.core.controller;
 import org.aksw.limes.core.evaluation.qualititativeMeasures.PseudoFMeasure;
 import org.aksw.limes.core.exceptions.UnsupportedMLImplementationException;
 import org.aksw.limes.core.io.cache.Cache;
-import org.aksw.limes.core.io.mapping.Mapping;
-import org.aksw.limes.core.ml.algorithm.ACoreMLAlgorithm;
-import org.aksw.limes.core.ml.algorithm.ActiveMLAlgorithm;
-import org.aksw.limes.core.ml.algorithm.MLAlgorithmFactory;
-import org.aksw.limes.core.ml.algorithm.MLImplementationType;
-import org.aksw.limes.core.ml.algorithm.SupervisedMLAlgorithm;
-import org.aksw.limes.core.ml.algorithm.UnsupervisedMLAlgorithm;
+import org.aksw.limes.core.io.mapping.AMapping;
+import org.aksw.limes.core.ml.algorithm.*;
 import org.aksw.limes.core.ml.oldalgorithm.MLModel;
 import org.aksw.limes.core.ml.setting.LearningParameters;
+
+import java.util.*;
 
 /**
  * Execution pipeline for generating mappings using ML.
  * Provides overloaded convenience methods.
+ *
  * @author Kevin Dreßler
  */
 public class MLPipeline {
 
-    public static Mapping execute (Cache source, Cache target, String mlAlgrorithmName, String mlImplementationType,
-                                   LearningParameters learningParameters, Mapping trainingData, PseudoFMeasure pfm) throws UnsupportedMLImplementationException {
+    public static AMapping execute(Cache source, Cache target, String mlAlgrorithmName, String mlImplementationType,
+                                   LearningParameters learningParameters, AMapping trainingData, PseudoFMeasure pfm) throws UnsupportedMLImplementationException {
         MLImplementationType mlType = MLAlgorithmFactory.getImplementationType(mlImplementationType);
         Class<? extends ACoreMLAlgorithm> clazz = MLAlgorithmFactory.getAlgorithmType(mlAlgrorithmName);
         MLModel mlm;
-        switch(mlType) {
+        switch (mlType) {
             case SUPERVISED_BATCH:
                 SupervisedMLAlgorithm mls = new SupervisedMLAlgorithm(clazz);
                 mls.init(learningParameters, source, target);
                 mlm = mls.learn(trainingData);
                 mls.predict(source, target, mlm);
             case SUPERVISED_ACTIVE:
+                // for active learning, need to reiterate and prompt the user for evaluation of examples:
+                boolean stopLearning = false;
                 ActiveMLAlgorithm mla = new ActiveMLAlgorithm(clazz);
-                mlm = mla.activeLearn(trainingData);
-                mla.predict(source, target, mlm);
+                mla.init(learningParameters, source, target);
+                Scanner scan = new Scanner(System.in);
+                Random random = new Random();
+                double rating = 0d;
+                do {
+                    mlm = mla.activeLearn(trainingData);
+                    Set<Map.Entry<String, HashMap<String, Double>>> entries = mlm.getMapping().getMap().entrySet();
+                    int i = 0;
+                    int j = random.nextInt(entries.size());
+
+                    for (Map.Entry<String, HashMap<String, Double>> entry : entries) {
+                        if (i == j) {
+                            int k = 0;
+                            int l = random.nextInt(entry.getValue().size());
+                            for (Map.Entry<String, Double> innerEntry : entry.getValue().entrySet()) {
+                                if (k == l) {
+
+                                }
+                                k++;
+                            }
+                        }
+                        i++;
+                    }
+
+
+                    mla.predict(source, target, mlm);
+
+                } while (!stopLearning);
             case UNSUPERVISED:
                 UnsupervisedMLAlgorithm mlu = new UnsupervisedMLAlgorithm(clazz);
                 mlu.init(learningParameters, source, target);
