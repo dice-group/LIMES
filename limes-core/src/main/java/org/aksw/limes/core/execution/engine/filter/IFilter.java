b24af2008cb7c4c69bbe8765f467a879e483c98f
Kleanthi Georgala <kleanthi@ainote6.informatik.uni-leipzig.de>
2016-07-20 15:44:55 +0200

    Updating Ifilter

--- limes-core/src/main/java/org/aksw/limes/core/execution/engine/filter/IFilter.java
+++ limes-core/src/main/java/org/aksw/limes/core/execution/engine/filter/IFilter.java
@@ -4,25 +4,23 @@ import org.aksw.limes.core.io.cache.Cache;
 import org.aksw.limes.core.io.mapping.AMapping;
 
 /**
- * IFilter implements the filter interface.
+ * Implements the filter interface.
  * 
  * @author Axel-C. Ngonga Ngomo (ngonga@informatik.uni-leipzig.de)
  * @author Kleanthi Georgala (georgala@informatik.uni-leipzig.de)
  * @version 1.0
  */
 
-
 public interface IFilter {
     /**
      * Naive filter function for mapping using a threshold as filtering
      * criterion.
      *
-     * @param map,
-     *            Map bearing the results of Link Specification
-     * @param threshold,
-     *            Value of threshold
-     * @return AMapping, Filtered mapping that satisfies sim {@literal >}=
-     *         threshold
+     * @param map
+     *            a map bearing the results of Link Specification
+     * @param threshold
+     *            the value of threshold
+     * @return a filtered mapping that satisfies sim {@literal >}= threshold
      */
     public AMapping filter(AMapping map, double threshold);
 
@@ -30,22 +28,22 @@ public interface IFilter {
      * Filter function for mapping using a condition and a threshold as
      * filtering criterion.
      *
-     * @param map,
-     *            Map bearing the results of Link Specification
-     * @param condition,
+     * @param map
+     *            a map bearing the results of Link Specification
+     * @param condition
      *            the condition for filtering
-     * @param threshold,
-     *            Value of threshold
-     * @param source,
-     *            Source Knowledge base
-     * @param target,
-     *            Target Knowledge base
-     * @param sourceVar,
-     *            Source property
-     * @param targetVar,
-     *            Target property
-     * @return AMapping, Filtered mapping that satisfies both the condition and
-     *         the threshold
+     * @param threshold
+     *            value of threshold
+     * @param source
+     *            source knowledge base
+     * @param target
+     *            target knowledge base
+     * @param sourceVar
+     *            source property
+     * @param targetVar
+     *            target property
+     * @return a filtered mapping that satisfies both the condition and the
+     *         threshold
      */
     public AMapping filter(AMapping map, String condition, double threshold, Cache source, Cache target,
             String sourceVar, String targetVar);
@@ -54,24 +52,24 @@ public interface IFilter {
      * Filter function for mapping using a condition and two thresholds as
      * filtering criterion.
      *
-     * @param map,
-     *            Map bearing the results of Link Specification
-     * @param condition,
+     * @param map
+     *            a map bearing the results of Link Specification
+     * @param condition
      *            the condition for filtering
-     * @param threshold,
-     *            Value of the first threshold
-     * @param mainThreshold,
-     *            Value of second threshold
-     * @param source,
-     *            Source Knowledge base
-     * @param target,
-     *            Target Knowledge base
-     * @param sourceVar,
-     *            Source property
-     * @param targetVar,
-     *            Target property
-     * @return AMapping, Filtered mapping that satisfies both the condition and
-     *         the thresholds
+     * @param threshold
+     *            value of the first threshold
+     * @param mainThreshold
+     *            value of second threshold
+     * @param source
+     *            source knowledge base
+     * @param target
+     *            target knowledge base
+     * @param sourceVar
+     *            source property
+     * @param targetVar
+     *            target property
+     * @return a filtered mapping that satisfies both the condition and the
+     *         thresholds
      */
     public AMapping filter(AMapping map, String condition, double threshold, double mainThreshold, Cache source,
             Cache target, String sourceVar, String targetVar);
@@ -80,24 +78,24 @@ public interface IFilter {
      * Reverse filter function for mapping using a condition and two thresholds
      * as filtering criterion.
      *
-     * @param map,
-     *            Map bearing the results of Link Specification
-     * @param condition,
+     * @param map
+     *            a map bearing the results of Link Specification
+     * @param condition
      *            the condition for filtering
-     * @param threshold,
-     *            Value of the first threshold
-     * @param mainThreshold,
-     *            Value of second threshold
-     * @param source,
-     *            Source Knowledge base
-     * @param target,
-     *            Target Knowledge base
-     * @param sourceVar,
-     *            Source property
-     * @param targetVar,
-     *            Target property
-     * @return Mapping, Filtered mapping that satisfies both the condition and
-     *         the thresholds
+     * @param threshold
+     *            value of the first threshold
+     * @param mainThreshold
+     *            value of second threshold
+     * @param source
+     *            source knowledge base
+     * @param target
+     *            target knowledge base
+     * @param sourceVar
+     *            source property
+     * @param targetVar
+     *            target property
+     * @return a filtered mapping that satisfies both the condition and the
+     *         thresholds
      */
     public AMapping reversefilter(AMapping map, String condition, double threshold, double mainThreshold, Cache source,
             Cache target, String sourceVar, String targetVar);
@@ -108,21 +106,21 @@ public interface IFilter {
      * expression (a*sim1)*(b*sim2) {@literal >}= t, which is not likely to be
      * used.
      *
-     * @param map1,
-     *            Map bearing the results of sim1 {@literal >}= (t-b)/a for add,
-     *            sim1 {@literal >}= t/(a*b) for mult
-     * @param map2,
-     *            Map bearing the results of sim2 {@literal >}= (t-a)/b for add,
-     *            sim2 {@literal >}= t/(a*b) for mult
-     * @param coef1,
-     *            Value of first coefficient
-     * @param coef2,
-     *            Value of second coefficient
-     * @param threshold,
-     *            Value of t
-     * @param operation,
-     *            Mathematical operation
-     * @return Mapping, Filtered mapping that satisfies a*sim1 + b*sim2
+     * @param map1
+     *            a Map bearing the results of sim1 {@literal >}= (t-b)/a for
+     *            add, sim1 {@literal >}= t/(a*b) for mult
+     * @param map2
+     *            a map bearing the results of sim2 {@literal >}= (t-a)/b for
+     *            add, sim2 {@literal >}= t/(a*b) for mult
+     * @param coef1
+     *            value of first coefficient
+     * @param coef2
+     *            value of second coefficient
+     * @param threshold
+     *            value of threshold
+     * @param operation
+     *            a mathematical operation
+     * @return a filtered mapping that satisfies a*sim1 + b*sim2
      *         {@literal >}= t for add, (a*sim1)*(b*sim2) {@literal >}= t for
      *         mult
      */
