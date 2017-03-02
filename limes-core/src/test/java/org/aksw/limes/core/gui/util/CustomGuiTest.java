25a7591d45b8940967e443d12bef9c9f6a6d4151
Daniel Obraczka <daniel@obraczka.de>
2017-03-02 09:49:58 +0100

    Fix another bug in CustomGuiTest

--- limes-core/src/test/java/org/aksw/limes/core/gui/util/CustomGuiTest.java
+++ limes-core/src/test/java/org/aksw/limes/core/gui/util/CustomGuiTest.java
@@ -13,79 +13,80 @@ import javafx.scene.control.TableView;
 
 public class CustomGuiTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(CustomGuiTest.class);
-	
-	public static boolean waitUntilNodeIsNotNull(String nodeId, int timeout){
+	private static final Logger logger = LoggerFactory.getLogger(CustomGuiTest.class);
+
+	public static boolean waitUntilNodeIsNotNull(String nodeId, int timeout) {
 		Node node = new FxRobot().lookup(nodeId).query();
-		while(node == null && timeout != 0)
-		{
-		  try {
-			  node = new FxRobot().lookup(nodeId).query();
-			  timeout --;
-			  Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			logger.error("Interrupted while waiting for Node to be not null!");
-			e.printStackTrace();
-		} catch(NullPointerException e){
-			logger.error("Maximum timeout reached, while waiting for Node to be not null!");
-			e.printStackTrace();
+		while (node == null && timeout != 0) {
+			try {
+				node = new FxRobot().lookup(nodeId).query();
+				timeout--;
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				logger.error("Interrupted while waiting for Node to be not null!");
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				logger.error("Maximum timeout reached, while waiting for Node to be not null!");
+				e.printStackTrace();
+			}
 		}
+		if (node != null) {
+			return true;
 		}
-		if(node != null){
-			return false;
-		}
+		logger.error("Maximum timeout reached, while waiting for Node to be not null!");
 		return false;
 	}
-	
+
 	/**
-	 * Uses FxRobot from TestFX to lookup the node 
-	 * @param nodeId ID of the node
-	 * @param timeout in seconds
+	 * Uses FxRobot from TestFX to lookup the node
+	 * 
+	 * @param nodeId
+	 *            ID of the node
+	 * @param timeout
+	 *            in seconds
 	 */
-	public static void waitUntilNodeIsVisible(String nodeId, int timeout){
+	public static void waitUntilNodeIsVisible(String nodeId, int timeout) {
 		Node node = new FxRobot().lookup(nodeId).query();
 		boolean found = false;
-		if(node == null){
+		if (node == null) {
 			found = waitUntilNodeIsNotNull(nodeId, timeout);
 		}
-		if(!found)
-			return;
-		node = new FxRobot().lookup(nodeId).query();
-		while(!node.isVisible() && timeout != 0)
-		{
-		  try {
-			  timeout --;
-			  Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			logger.error("Interrupted while waiting for Node to be visible!");
-			e.printStackTrace();
-		} catch(NullPointerException e){
+		if (!found) {
 			logger.error("Maximum timeout reached, while waiting for Node to be visible!");
-			e.printStackTrace();
+			return;
 		}
+		node = new FxRobot().lookup(nodeId).query();
+		while (!node.isVisible() && timeout != 0) {
+			try {
+				timeout--;
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				logger.error("Interrupted while waiting for Node to be visible!");
+				e.printStackTrace();
+			}
 		}
-		
+
 	}
-	
+
 	public static TableRow<?> getFirstRowOfTableView(String tableSelector) {
 		TableView<?> tableView = new FxRobot().lookup(tableSelector).query();
 
-	    List<Node> current = tableView.getChildrenUnmodifiable();
-	    while (current.size() == 1) {
-	        current = ((Parent) current.get(0)).getChildrenUnmodifiable();
-	    }
+		List<Node> current = tableView.getChildrenUnmodifiable();
+		while (current.size() == 1) {
+			current = ((Parent) current.get(0)).getChildrenUnmodifiable();
+		}
 
-	    current = ((Parent) current.get(1)).getChildrenUnmodifiable();
-	    while (!(current.get(0) instanceof TableRow)) {
-	        current = ((Parent) current.get(0)).getChildrenUnmodifiable();
-	    }
+		current = ((Parent) current.get(1)).getChildrenUnmodifiable();
+		while (!(current.get(0) instanceof TableRow)) {
+			current = ((Parent) current.get(0)).getChildrenUnmodifiable();
+		}
 
-	    Node node = current.get(0);
-	    if (node instanceof TableRow) {
-	        return (TableRow<?>) node;
-	    } else {
-	        throw new RuntimeException("Expected Group with only TableRows as children");
-	    }
+		Node node = current.get(0);
+		if (node instanceof TableRow) {
+			return (TableRow<?>) node;
+		} else {
+			throw new RuntimeException("Expected Group with only TableRows as children");
+		}
 	}
 
 }
