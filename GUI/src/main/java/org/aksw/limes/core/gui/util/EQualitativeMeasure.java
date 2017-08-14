5344359c3339383aae679fef2c9af3a65a0f00fc
Daniel Obraczka <daniel@obraczka.de>
2017-08-14 14:18:53 +0200

    Delete old folder

--- GUI/src/main/java/org/aksw/limes/core/gui/util/EQualitativeMeasure.java
+++
@@ -1,6 +0,0 @@
-package org.aksw.limes.core.gui.util;
-
-public enum EQualitativeMeasure {
-    Accuracy, PseudoFMeasure, PseudoRefFMeasure, PseudoPrecision, PseudoRefPrecision, PseudoRecall, PseudoRefRecall, FMeasure, Precision, Recall
-
-}
