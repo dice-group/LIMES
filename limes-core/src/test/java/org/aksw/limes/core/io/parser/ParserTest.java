2d51251a937bf44ba6562a2806cb90db002974fb
Daniel Obraczka <daniel@obraczka.de>
2017-11-03 10:49:53 +0100

    Fixes #115

--- limes-core/src/test/java/org/aksw/limes/core/io/parser/ParserTest.java
+++ limes-core/src/test/java/org/aksw/limes/core/io/parser/ParserTest.java
@@ -1,7 +1,9 @@
 package org.aksw.limes.core.io.parser;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.aksw.limes.core.exceptions.UnsupportedOperator;
 import org.junit.Test;
@@ -75,6 +77,39 @@ public class ParserTest {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+    
+    @Test
+    public void weightedTest(){
+    	double theta = 0.8;
+    	Parser p = new Parser("ADD("
+    							+ "0.5* ADD("
+    								+ "0.8* ExactMatch(a.purl:identifier, b.sg:hasPerson),"
+    								+ "0.2* Jaro(a.foaf:name, b.sg:publishedName)),"
+    							+ "0.5* ADD("
+    								+ "0.8* ExactMatch(a.DBgridID, b.SGgridID),"
+    								+ "0.2* Jaro(a.foaf:name, b.SGgridName)))",theta);
+    	assertEquals("ADD(0.8*ExactMatch(a.purl:identifier,b.sg:hasPerson),0.2*Jaro(a.foaf:name,b.sg:publishedName))",p.getLeftTerm());
+    	assertEquals("ADD(0.8*ExactMatch(a.DBgridID,b.SGgridID),0.2*Jaro(a.foaf:name,b.SGgridName))",p.getRightTerm());
+    	assertEquals(0.5,p.getLeftCoefficient(),0.0);
+    	assertEquals(0.5,p.getRightCoefficient(),0.0);
+        Parser p1 = new Parser(p.getLeftTerm(), Math.abs(theta - p.getRightCoefficient()) / p.getLeftCoefficient());
+    	assertEquals("ExactMatch(a.purl:identifier,b.sg:hasPerson)", p1.getLeftTerm());
+    	assertEquals("Jaro(a.foaf:name,b.sg:publishedName)",p1.getRightTerm());
+    	assertEquals(0.8,p1.getLeftCoefficient(),0.0);
+    	assertEquals(0.2,p1.getRightCoefficient(),0.0);
+        Parser p2 = new Parser(p.getRightTerm(), Math.abs(theta - p.getLeftCoefficient()) / p.getRightCoefficient());
+        assertEquals("ExactMatch(a.DBgridID,b.SGgridID)",p2.getLeftTerm());
+        assertEquals("Jaro(a.foaf:name,b.SGgridName)", p2.getRightTerm());
+    	assertEquals(0.8,p2.getLeftCoefficient(),0.0);
+    	assertEquals(0.2,p2.getRightCoefficient(),0.0);
+    }
+    
+    @Test
+    public void weih2(){
+    	Parser p = new Parser("ADD(0.3*trigrams(x.rdfs:label,y.dc:title)|0.3, 0.7*euclidean(x.lat|x.long,y.latitude|y.longitude)|0.5)",0.8);
+    	System.out.println(p.getLeftCoefficient());
+    	System.out.println(p.getRightCoefficient());
     }
 
 }
