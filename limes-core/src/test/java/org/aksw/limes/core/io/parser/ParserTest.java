d9da7e6f87e52fd96310d425fcb3484ece10fcd0
Daniel Obraczka <daniel@obraczka.de>
2017-11-07 14:45:56 +0100

    Minor code cleanup

--- limes-core/src/test/java/org/aksw/limes/core/io/parser/ParserTest.java
+++ limes-core/src/test/java/org/aksw/limes/core/io/parser/ParserTest.java
@@ -1,9 +1,8 @@
 package org.aksw.limes.core.io.parser;
 
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.aksw.limes.core.exceptions.UnsupportedOperator;
 import org.junit.Test;
