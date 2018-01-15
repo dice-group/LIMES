5ff4d820b060c9c3fdc8c4aed427869c3be4b03e
Swante Scholz <essmexp@gmail.com>
2018-01-15 07:26:44 +0100

    add wordnet vs wordembedding comparison test

--- limes-core/src/test/java/org/aksw/limes/core/measures/measure/string/bilang/WordEmbeddingsTest.java
+++ limes-core/src/test/java/org/aksw/limes/core/measures/measure/string/bilang/WordEmbeddingsTest.java
@@ -3,7 +3,12 @@ package org.aksw.limes.core.measures.measure.string.bilang;
 import static org.apache.jena.system.JenaSystem.forEach;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.aksw.limes.core.measures.measure.string.bilang.WordEmbeddings.Vectord;
 import org.apache.commons.math3.util.Pair;
 import org.junit.Before;
@@ -11,12 +16,7 @@ import org.junit.Test;
 
 public class WordEmbeddingsTest {
 
-  WordEmbeddings we = new WordEmbeddings();
-
-  @Before
-  public void setUp() {
-    we.readBilangDataFiles("src/test/resources/unsup.128");
-  }
+  WordEmbeddings we = new WordEmbeddings("src/test/resources/unsup.128");
 
   @Test
   public void testSimilarity() {
@@ -34,7 +34,8 @@ public class WordEmbeddingsTest {
     assertTrue(0.5 < we.getCosineSimilarityForWords("dog", "hund"));
     assertTrue(0.5 < we.getCosineSimilarityForWords("katze", "cat"));
     assertEquals(
-        we.getCosineSimilarityForWords("hund", "cat"), we.getCosineSimilarityForWords("katze", "dog"), 0.1);
+        we.getCosineSimilarityForWords("hund", "cat"),
+        we.getCosineSimilarityForWords("katze", "dog"), 0.1);
     assertTrue(we.getCosineSimilarityForWords("mammal", "hund") > we
         .getCosineSimilarityForWords("hund", "rocket"));
     assertTrue(we.getCosineSimilarityForWords("car", "rakete") > we
@@ -43,9 +44,10 @@ public class WordEmbeddingsTest {
 
   @Test
   public void testNearestNeighbors() {
-    List<Pair<String, Double>> neighbors = we.computeNNearestWords(we.getWordVector("germany"),50);
+    List<Pair<String, Double>> neighbors = we
+        .computeNNearestWords(we.getWordVector("university"), 50);
     neighbors.forEach(System.out::println);
-    assertTrue(neighbors.stream().anyMatch(it-> it.getKey().equals("niederlande")));
+    assertTrue(neighbors.stream().anyMatch(it -> it.getKey().equals("niederlande")));
   }
 
   @Test
@@ -53,9 +55,9 @@ public class WordEmbeddingsTest {
     Vectord a = we.getWordVector("man");
     Vectord b = we.getWordVector("woman");
     Vectord c = we.getWordVector("königin");
-    List<Pair<String, Double>> neighbors = we.computeNNearestWords(c.minus(b).plus(a),50);
+    List<Pair<String, Double>> neighbors = we.computeNNearestWords(c.minus(b).plus(a), 50);
     neighbors.forEach(System.out::println);
-    assertTrue(neighbors.stream().anyMatch(it-> it.getKey().equals("king")));
+    assertTrue(neighbors.stream().anyMatch(it -> it.getKey().equals("king")));
   }
 
   @Test
@@ -63,18 +65,69 @@ public class WordEmbeddingsTest {
     Vectord a = we.getWordVector("germany");
     Vectord b = we.getWordVector("berlin");
     Vectord c = we.getWordVector("frankreich");
-    List<Pair<String, Double>> neighbors = we.computeNNearestWords(c.minus(a).plus(b),50);
+    List<Pair<String, Double>> neighbors = we.computeNNearestWords(c.minus(a).plus(b), 50);
     neighbors.forEach(System.out::println);
-    assertTrue(neighbors.stream().anyMatch(it-> it.getKey().equals("paris")));
+    assertTrue(neighbors.stream().anyMatch(it -> it.getKey().equals("paris")));
   }
 
   @Test
   public void testSum() {
     Vectord a = we.getWordVector("water");
     Vectord b = we.getWordVector("flugzeug");
-    List<Pair<String, Double>> neighbors = we.computeNNearestWords(a.plus(b),50);
+    List<Pair<String, Double>> neighbors = we.computeNNearestWords(a.plus(b), 50);
     neighbors.forEach(System.out::println);
-    assertTrue(neighbors.stream().anyMatch(it-> it.getKey().equals("ship")));
+    assertTrue(neighbors.stream().anyMatch(it -> it.getKey().equals("ship")));
+  }
+
+  @Test
+  public void testCompareVersusWordNet() {
+    try {
+      ArrayList<String> words1 = new ArrayList<>();
+      ArrayList<String> words2 = new ArrayList<>();
+      ArrayList<Double> similarities = new ArrayList<>();
+      Stream<String> lines = Files.lines(Paths.get("src/test/resources/wordsim352.tab"));
+      lines.forEach(line -> {
+        String[] parts = line.split("\t");
+        String word1 = parts[0];
+        String word2 = parts[1];
+        double similarity = Double.parseDouble(parts[2]);
+        words1.add(word1);
+        words2.add(word2);
+        similarities.add(similarity);
+      });
+      lines.close();
+      WordNetInterface wn = new WordNetInterface("src/test/resources/WordNet-3.0");
+      int size = words1.size();
+      int weCorrect = 0;
+      int wnCorrect = 0;
+      for (int i = 0; i < size; i += 2) {
+        String a = words1.get(i);
+        String b = words2.get(i);
+        String c = words1.get(i + 1);
+        String d = words2.get(i + 1);
+        double sim1 = similarities.get(i);
+        double sim2 = similarities.get(i + 1);
+        System.out.println(a + " " + b + " " + c + " " + d + " " + sim1 + " " + sim2);
+        if (sim1 < sim2 == we.getCosineSimilarityForWords(a, b) < we
+            .getCosineSimilarityForWords(c, d)) {
+          weCorrect++;
+          System.out.println("word2vec correct");
+        } else {
+          System.out.println("word2vec incorrect");
+        }
+        if (sim1 < sim2 == wn.computeWuPalmerSimilarity(a,b) < wn.computeWuPalmerSimilarity(c,d)) {
+          wnCorrect++;
+          System.out.println("wordnet correct");
+        } else {
+          System.out.println("wordnet incorrect");
+        }
+
+      }
+      System.out.println("word2vec score (out of " + (size/2) + "): " + weCorrect);
+      System.out.println("WordNet score (out of " + (size/2) + "): " + wnCorrect);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
