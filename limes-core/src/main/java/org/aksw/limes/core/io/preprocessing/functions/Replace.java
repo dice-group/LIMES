0fce3da3bcf2f9f12f1d1a6cfe79806ee239347a
Daniel Obraczka <daniel@obraczka.de>
2018-02-22 12:12:58 +0100

    Enhanced preprocessing
    - Added possibility to add arguments to functions
    - Added replace function

---
+++ limes-core/src/main/java/org/aksw/limes/core/io/preprocessing/functions/Replace.java
@@ -0,0 +1,45 @@
+package org.aksw.limes.core.io.preprocessing.functions;
+
+import java.util.TreeSet;
+
+import org.aksw.limes.core.io.cache.Instance;
+import org.aksw.limes.core.io.preprocessing.AProcessingFunction;
+import org.aksw.limes.core.io.preprocessing.IProcessingFunction;
+
+public class Replace extends AProcessingFunction implements IProcessingFunction {
+
+	@Override
+	public Instance applyFunction(Instance i, String[] properties, String... arguments) {
+		for(String prop: properties){
+            TreeSet<String> oldValues = i.getProperty(prop);
+            TreeSet<String> newValues = new TreeSet<>();
+            for (String value : oldValues) {
+                newValues.add(value.replace(arguments[0],arguments[1]));
+            }
+            i.replaceProperty(prop, newValues);
+		}
+		return i;
+	
+	}
+
+	@Override
+	public int minNumberOfProperties() {
+		return 1;
+	}
+
+	@Override
+	public int maxNumberOfProperties() {
+		return -1;
+	}
+
+	@Override
+	public int minNumberOfArguments() {
+		return 2;
+	}
+
+	@Override
+	public int maxNumberOfArguments() {
+		return 2;
+	}
+
+}
