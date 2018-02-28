56e27db584041d23bec4697af09c4e8dd1aa1ab7
Daniel Obraczka <daniel@obraczka.de>
2018-02-28 21:26:12 +0100

    Removed previously missed use of preprocessor in CsvQueryModule

--- limes-core/src/main/java/org/aksw/limes/core/io/query/CsvQueryModule.java
+++ limes-core/src/main/java/org/aksw/limes/core/io/query/CsvQueryModule.java
@@ -55,7 +55,6 @@ public class CsvQueryModule implements IQueryModule {
                 properties.addAll(Arrays.asList(split));
 
                 s = reader.readLine();
-                String rawValue;
                 String id, value;
                 while (s != null) {
                     //split = s.split(SEP);
@@ -64,16 +63,8 @@ public class CsvQueryModule implements IQueryModule {
 
                     id = split[0];
                     for (String propertyLabel : kb.getProperties()) {
-//                    	System.out.println("Trying to access property "+propertyLabel+" at position "+properties.indexOf(propertyLabel));
-                        rawValue = split[properties.indexOf(propertyLabel)];
-                        for (String propertyDub : kb.getFunctions().get(propertyLabel).keySet()) {
-                            //functions.get(propertyLabel).get(propertyDub) gets the preprocessing chain that leads from 
-                            //the propertyLabel to the propertyDub
-                            value = Preprocessor.process(rawValue, kb.getFunctions().get(propertyLabel).get(propertyDub));
-                            if (properties.indexOf(propertyLabel) >= 0) {
-                                c.addTriple(id, propertyDub, value);
-                            }
-                        }
+                            value = split[properties.indexOf(propertyLabel)];
+                            c.addTriple(id, propertyLabel, value);
                     }
                     s = reader.readLine();
                 }
@@ -115,7 +106,6 @@ public class CsvQueryModule implements IQueryModule {
 
                 kb.setProperties(properties);
                 s = reader.readLine();
-                String rawValue;
                 String id, value;
                 while (s != null) {
                     split = s.split(SEP);
@@ -123,19 +113,8 @@ public class CsvQueryModule implements IQueryModule {
                     id = split[0].substring(1, split[0].length() - 1);
                     //logger.info(id);
                     for (String propertyLabel : kb.getProperties()) {
-                        rawValue = split[properties.indexOf(propertyLabel)];
-                        if (kb.getFunctions().containsKey(propertyLabel)) {
-                            for (String propertyDub : kb.getFunctions().get(propertyLabel).keySet()) {
-                                //functions.get(propertyLabel).get(propertyDub) gets the preprocessing chain that leads from 
-                                //the propertyLabel to the propertyDub
-                                value = Preprocessor.process(rawValue, kb.getFunctions().get(propertyLabel).get(propertyDub));
-                                if (properties.indexOf(propertyLabel) >= 0) {
-                                    c.addTriple(id, propertyDub, value);
-                                }
-                            }
-                        } else {
-                            c.addTriple(id, propertyLabel, rawValue.replaceAll(Pattern.quote("@en"), ""));
-                        }
+                        value = split[properties.indexOf(propertyLabel)];
+                        c.addTriple(id, propertyLabel, value.replaceAll(Pattern.quote("@en"), ""));
                     }
                     s = reader.readLine();
                 }
