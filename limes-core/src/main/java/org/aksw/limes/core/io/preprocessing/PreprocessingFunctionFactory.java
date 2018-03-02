3ba4f1e5d1b8c75edd072c52a349d3554ab677c5
Daniel Obraczka <daniel@obraczka.de>
2018-03-02 16:16:13 +0100

    Added Factory and Enum for Preprocessing functions

---
+++ limes-core/src/main/java/org/aksw/limes/core/io/preprocessing/PreprocessingFunctionFactory.java
@@ -0,0 +1,91 @@
+package org.aksw.limes.core.io.preprocessing;
+
+import org.aksw.limes.core.exceptions.InvalidPreprocessingFunctionException;
+import org.aksw.limes.core.io.preprocessing.functions.CleanIri;
+import org.aksw.limes.core.io.preprocessing.functions.CleanNumber;
+import org.aksw.limes.core.io.preprocessing.functions.Concat;
+import org.aksw.limes.core.io.preprocessing.functions.RegexReplace;
+import org.aksw.limes.core.io.preprocessing.functions.RemoveLanguageTag;
+import org.aksw.limes.core.io.preprocessing.functions.RenameProperty;
+import org.aksw.limes.core.io.preprocessing.functions.Replace;
+import org.aksw.limes.core.io.preprocessing.functions.ToCelsius;
+import org.aksw.limes.core.io.preprocessing.functions.ToFahrenheit;
+import org.aksw.limes.core.io.preprocessing.functions.ToLowercase;
+import org.aksw.limes.core.io.preprocessing.functions.ToUppercase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PreprocessingFunctionFactory {
+    static Logger logger = LoggerFactory.getLogger(PreprocessingFunctionFactory.class);
+
+    public static final String CLEAN_IRI = "cleaniri";
+    public static final String CLEAN_NUMBER = "number";
+    public static final String CONCAT = "concat";
+    public static final String REGEX_REPLACE = "regexreplace";
+    public static final String REMOVE_LANGUAGE_TAG = "nolang";
+    public static final String RENAME_PROPERTY = "rename";
+    public static final String REPLACE = "replace";
+    public static final String TO_CELSIUS = "celsius";
+    public static final String TO_FAHRENHEIT = "fahrenheit";
+    public static final String TO_UPPERCASE = "uppercase";
+    public static final String TO_LOWERCASE = "lowercase";
+    
+    public static PreprocessingFunctionType getPreprocessingType(String expression){
+    	switch(expression){
+            case(CLEAN_IRI): 
+            	return PreprocessingFunctionType.CLEAN_IRI;
+            case(CLEAN_NUMBER): 
+            	return PreprocessingFunctionType.CLEAN_NUMBER;
+            case(CONCAT): 
+            	return PreprocessingFunctionType.CONCAT;
+            case(REGEX_REPLACE): 
+            	return PreprocessingFunctionType.REGEX_REPLACE;
+            case(REMOVE_LANGUAGE_TAG): 
+            	return PreprocessingFunctionType.REMOVE_LANGUAGE_TAG;
+            case(RENAME_PROPERTY): 
+            	return PreprocessingFunctionType.RENAME_PROPERTY;
+            case(REPLACE): 
+            	return PreprocessingFunctionType.REPLACE;
+            case(TO_CELSIUS): 
+            	return PreprocessingFunctionType.TO_CELSIUS;
+            case(TO_FAHRENHEIT): 
+            	return PreprocessingFunctionType.TO_FAHRENHEIT;
+            case(TO_UPPERCASE): 
+            	return PreprocessingFunctionType.TO_UPPERCASE;
+            case(TO_LOWERCASE): 
+            	return PreprocessingFunctionType.TO_LOWERCASE;
+    		default:
+    			throw new InvalidPreprocessingFunctionException(expression);
+    	}
+    }
+    
+   public static APreprocessingFunction getPreprocessingFunction(PreprocessingFunctionType type){
+	    switch(type){
+            case CLEAN_IRI: 
+            	return new CleanIri();
+            case CLEAN_NUMBER: 
+            	return new CleanNumber();
+            case CONCAT: 
+            	return new Concat();
+            case REGEX_REPLACE: 
+            	return new RegexReplace();
+            case REMOVE_LANGUAGE_TAG: 
+            	return new RemoveLanguageTag();
+            case RENAME_PROPERTY: 
+            	return new RenameProperty();
+            case REPLACE: 
+            	return new Replace();
+            case TO_CELSIUS: 
+            	return new ToCelsius();
+            case TO_FAHRENHEIT: 
+            	return new ToFahrenheit();
+            case TO_UPPERCASE: 
+            	return new ToUppercase();
+            case TO_LOWERCASE: 
+            	return new ToLowercase();
+            default:
+    			throw new InvalidPreprocessingFunctionException(type.toString());
+	    }
+   }
+    
+}
---
+++ limes-core/src/main/java/org/aksw/limes/core/io/preprocessing/PreprocessingFunctionType.java
@@ -0,0 +1,6 @@
+package org.aksw.limes.core.io.preprocessing;
+
+public enum PreprocessingFunctionType {
+	CLEAN_IRI, CLEAN_NUMBER, CONCAT, REGEX_REPLACE, REMOVE_LANGUAGE_TAG, RENAME_PROPERTY, 
+	REPLACE, TO_CELSIUS, TO_FAHRENHEIT, TO_UPPERCASE, TO_LOWERCASE
+}
