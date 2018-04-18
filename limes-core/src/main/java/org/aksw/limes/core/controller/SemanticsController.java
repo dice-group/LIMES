74099307d3875147082d3bbd79ae362586cc34d9
Kleanthi Georgala <kleanthi@ainote6.informatik.uni-leipzig.de>
2018-04-18 12:50:03 +0100

    Fixing pom

--- limes-core/src/main/java/org/aksw/limes/core/controller/SemanticsController.java
+++ limes-core/src/main/java/org/aksw/limes/core/controller/SemanticsController.java
@@ -4,7 +4,6 @@ import org.aksw.limes.core.io.config.Configuration;
 import org.aksw.limes.core.io.config.reader.AConfigurationReader;
 import org.aksw.limes.core.io.config.reader.rdf.RDFConfigurationReader;
 import org.aksw.limes.core.io.config.reader.xml.XMLConfigurationReader;
-import org.apache.commons.cli.CommandLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,19 +11,10 @@ public class SemanticsController {
 
     static Logger logger = LoggerFactory.getLogger(SemanticsController.class);
 
-    public static Configuration getConfig(CommandLine cmd) {
+    public static Configuration getConfig(String[] args) {
         // 1. Determine appropriate ConfigurationReader
-        String format = "xml";
-        String fileNameOrUri = cmd.getArgs()[0];
-        if (cmd.hasOption('f')) {
-            format = cmd.getOptionValue("f").toLowerCase();
-        } else if (fileNameOrUri.endsWith(".nt")
-                    || fileNameOrUri.endsWith(".ttl")
-                    || fileNameOrUri.endsWith(".n3")
-                    || fileNameOrUri.endsWith(".rdf")) {
-            format = "rdf";
-        }
-
+        String fileNameOrUri = args[0];
+        String format = fileNameOrUri.split(".")[1];
         AConfigurationReader reader = null;
         switch (format) {
             case "xml":
@@ -37,7 +27,6 @@ public class SemanticsController {
                 logger.error("Error:\n\t Not a valid format: \"" + format + "\"!");
                 System.exit(1);
         }
-
         return reader.read();
     }
 }
