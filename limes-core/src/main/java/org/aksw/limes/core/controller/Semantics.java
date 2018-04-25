9b6d2e4fe0b7c4473aaa594d123114cc929e0939
Kleanthi Georgala <kleanthi@ainote6.informatik.uni-leipzig.de>
2018-04-25 11:10:54 +0100

    Update code

--- limes-core/src/main/java/org/aksw/limes/core/controller/Semantics.java
+++ limes-core/src/main/java/org/aksw/limes/core/controller/Semantics.java
@@ -1,18 +1,13 @@
 package org.aksw.limes.core.controller;
 
 import com.google.common.collect.Lists;
-import com.google.common.io.Files;
 import com.opencsv.CSVWriter;
 
 import org.aksw.limes.core.datastrutures.GoldStandard;
 import org.aksw.limes.core.evaluation.evaluationDataLoader.DataSetChooser;
-import org.aksw.limes.core.evaluation.evaluationDataLoader.DataSetChooser.DataSets;
 import org.aksw.limes.core.evaluation.evaluator.EvaluatorType;
 import org.aksw.limes.core.evaluation.evaluationDataLoader.EvaluationData;
-import org.aksw.limes.core.evaluation.qualititativeMeasures.FMeasure;
-import org.aksw.limes.core.evaluation.qualititativeMeasures.Precision;
 import org.aksw.limes.core.evaluation.qualititativeMeasures.QualitativeMeasuresEvaluator;
-import org.aksw.limes.core.evaluation.qualititativeMeasures.Recall;
 import org.aksw.limes.core.exceptions.UnsupportedMLImplementationException;
 import org.aksw.limes.core.execution.engine.ExecutionEngine;
 import org.aksw.limes.core.execution.engine.ExecutionEngineFactory;
@@ -28,13 +23,20 @@ import org.aksw.limes.core.io.ls.LinkSpecification;
 import org.aksw.limes.core.io.mapping.AMapping;
 import org.aksw.limes.core.io.mapping.MappingFactory;
 import org.aksw.limes.core.measures.mapper.MappingOperations;
+import org.aksw.limes.core.measures.measure.MeasureType;
+import org.aksw.limes.core.ml.algorithm.LearningParameter;
+import org.aksw.limes.core.ml.algorithm.MLAlgorithmFactory;
+import org.aksw.limes.core.ml.algorithm.MLImplementationType;
+import org.aksw.limes.core.ml.algorithm.MLResults;
+import org.aksw.limes.core.ml.algorithm.SupervisedMLAlgorithm;
+import org.aksw.limes.core.ml.algorithm.WombatSimple;
+import org.aksw.limes.core.ml.algorithm.wombat.AWombat;
 import org.apache.commons.lang3.time.StopWatch;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.*;
 
 /**
@@ -55,9 +57,12 @@ public class Semantics {
     public static AMapping fullReferenceMapping;
     public static int fold = 1;
     public static String datasetName;
-    public static String resultsFile = null;
+    public static String resultsFileTest = null;
+    public static String resultsFileTraining = null;
     public static EvaluationData data = null;
-    public static CSVWriter csvWriter = null;
+    public static CSVWriter csvWriterTest = null;
+    public static CSVWriter csvWriterTrain = null;
+
     public static int experiment = 0;
 
     public static void init(String[] args) {
@@ -67,10 +72,16 @@ public class Semantics {
 
         data = DataSetChooser.getData(datasetName);
 
-        resultsFile = data.getDatasetFolder() + data.getEvaluationResultFileName();
+        resultsFileTest = data.getDatasetFolder() + "Test" + data.getEvaluationResultFileName();
+        resultsFileTraining = data.getDatasetFolder() + "Training" + data.getEvaluationResultFileName();
 
-        // create file if it doesn't exist
-        File f = new File(resultsFile);
+        createFile(resultsFileTest, csvWriterTest);
+        createFile(resultsFileTraining, csvWriterTrain);
+
+    }
+
+    public static void createFile(String fileName, CSVWriter writer) {
+        File f = new File(fileName);
         if (!f.exists()) {
             try {
                 f.createNewFile();
@@ -80,24 +91,70 @@ public class Semantics {
                 throw new RuntimeException();
             }
         }
+
         // write header
         try {
-            csvWriter = new CSVWriter(new FileWriter(resultsFile, true));
+            writer = new CSVWriter(new FileWriter(fileName, true));
         } catch (IOException e) {
             logger.error("Can't create csv writer");
             e.printStackTrace();
             throw new RuntimeException();
         }
-        csvWriter.writeNext(new String[] { "GoldStandard", "Predictions", "Precision", "Recall", "F-measure",
-                "Accuracy", "Runtime", "LS"}, false);
+        writer.writeNext(new String[] { "GoldStandard", "Predictions", "Precision", "Recall", "F-measure", "Accuracy",
+                "Runtime", "LS" }, false);
         try {
-            csvWriter.close();
+            writer.close();
         } catch (IOException e) {
             logger.error("Couldn't close csv writer");
             e.printStackTrace();
             throw new RuntimeException();
         }
+    }
 
+    public static List<LearningParameter> setLearningParameters(Set<String> measures) {
+        List<LearningParameter> learningParameters = new ArrayList<LearningParameter>();
+        long maxRefineTreeSize = 100;
+        int maxIterationNumber = 10;
+        int maxIterationTimeInMin = 5;
+        int maxExecutionTimeInMin = 50;
+        double maxFitnessThreshold = 1;
+        double childrenPenaltyWeight = 1;
+        double complexityPenaltyWeight = 1;
+        boolean saveMapping = true;
+        double minPropertyCoverage = 0.6;
+        double propertyLearningRate = 0.9;
+        double overallPenaltyWeight = 0.5d;
+        boolean verbose = false;
+
+        learningParameters = new ArrayList<>();
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_MAX_REFINEMENT_TREE_SIZE, maxRefineTreeSize, Long.class,
+                10d, Long.MAX_VALUE, 10d, AWombat.PARAMETER_MAX_REFINEMENT_TREE_SIZE));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_MAX_ITERATIONS_NUMBER, maxIterationNumber, Integer.class,
+                1d, Integer.MAX_VALUE, 10d, AWombat.PARAMETER_MAX_ITERATIONS_NUMBER));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_MAX_ITERATION_TIME_IN_MINUTES, maxIterationTimeInMin,
+                Integer.class, 1d, Integer.MAX_VALUE, 1, AWombat.PARAMETER_MAX_ITERATION_TIME_IN_MINUTES));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_EXECUTION_TIME_IN_MINUTES, maxExecutionTimeInMin,
+                Integer.class, 1d, Integer.MAX_VALUE, 1, AWombat.PARAMETER_EXECUTION_TIME_IN_MINUTES));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_MAX_FITNESS_THRESHOLD, maxFitnessThreshold, Double.class,
+                0d, 1d, 0.01d, AWombat.PARAMETER_MAX_FITNESS_THRESHOLD));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_MIN_PROPERTY_COVERAGE, minPropertyCoverage, Double.class,
+                0d, 1d, 0.01d, AWombat.PARAMETER_MIN_PROPERTY_COVERAGE));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_PROPERTY_LEARNING_RATE, propertyLearningRate,
+                Double.class, 0d, 1d, 0.01d, AWombat.PARAMETER_PROPERTY_LEARNING_RATE));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_OVERALL_PENALTY_WEIGHT, overallPenaltyWeight,
+                Double.class, 0d, 1d, 0.01d, AWombat.PARAMETER_OVERALL_PENALTY_WEIGHT));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_CHILDREN_PENALTY_WEIGHT, childrenPenaltyWeight,
+                Double.class, 0d, 1d, 0.01d, AWombat.PARAMETER_CHILDREN_PENALTY_WEIGHT));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_COMPLEXITY_PENALTY_WEIGHT, complexityPenaltyWeight,
+                Double.class, 0d, 1d, 0.01d, AWombat.PARAMETER_COMPLEXITY_PENALTY_WEIGHT));
+        learningParameters
+                .add(new LearningParameter(AWombat.PARAMETER_VERBOSE, verbose, Boolean.class, 0, 1, 0, AWombat.PARAMETER_VERBOSE));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_ATOMIC_MEASURES, measures, MeasureType.class, 0, 0, 0,
+                AWombat.PARAMETER_ATOMIC_MEASURES));
+        learningParameters.add(new LearningParameter(AWombat.PARAMETER_SAVE_MAPPING, saveMapping, Boolean.class, 0, 1, 0,
+                AWombat.PARAMETER_SAVE_MAPPING));
+        
+        return learningParameters;
     }
 
     /**
@@ -106,7 +163,6 @@ public class Semantics {
      * @author sherif
      */
     public static void main(String[] args) throws UnsupportedMLImplementationException {
-        int mostInformativeExaplesCount = 10;
         // get training data
 
         Semantics.init(args);
@@ -119,30 +175,54 @@ public class Semantics {
             fold = i + 1;
             AMapping testSet = subSets.get(i);
             testSet.getReversedMap();
-            AMapping learningPool = getLearningPool(subSets, i);// training set
-            learningPool.getReversedMap();
-            fullReferenceMapping = learningPool;
-            List<ACache> learning = reduceCaches(learningPool, fullSourceCache, fullTargetCache);// give
-                                                                                                 // this
-                                                                                                 // to
-                                                                                                 // wombat
-            List<ACache> testing = reduceCaches(testSet, fullSourceCache, fullTargetCache);
-            AMapping trainingSet = learningPool;
-            // compare testSet with result from limes
-            // compare mapping from MLResults with the training set
+            AMapping trainingSet = getLearningPool(subSets, i);// training set
+            trainingSet.getReversedMap();
+            fullReferenceMapping = trainingSet;
+            // give this to wombat
+            List<ACache> trainingCaches = reduceCaches(trainingSet, fullSourceCache, fullTargetCache);
+            List<ACache> testingCaches = reduceCaches(testSet, fullSourceCache, fullTargetCache);
 
+            Set<String> measures = null;
+            List<LearningParameter> lp = null;
             switch (experiment) {
-            case 1:
 
+            case 1:
+                String[] strings = new String[] { "jaccard", "trigrams", "cosine", "qgrams", "overlap", "levenshtein" };
+                measures = new HashSet<>(Arrays.asList(strings));
                 break;
             case 2:
+                String[] semantic = new String[] { "shortest_path", "wupalmer", "li", "lch" };
+                measures = new HashSet<>(Arrays.asList(semantic));
 
                 break;
             case 3:
-
+                String[] measureSet = new String[] { "jaccard", "trigrams", "cosine", "qgrams", "overlap",
+                        "levenshtein", "shortest_path", "wupalmer", "li", "lch" };
+                measures = new HashSet<>(Arrays.asList(measureSet));
                 break;
 
             }
+            lp = setLearningParameters(measures);
+
+            // create wombat and let it learn
+            SupervisedMLAlgorithm wombat = MLAlgorithmFactory
+                    .createMLAlgorithm(WombatSimple.class, MLImplementationType.SUPERVISED_BATCH).asSupervised();
+            wombat.init(lp, trainingCaches.get(0), trainingCaches.get(1));
+            MLResults mlResults = wombat.learn(trainingSet);
+
+            // execute ls
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+            AMapping predictions = executeLinkSpecs(mlResults.getLinkSpecification(), testingCaches.get(0),
+                    testingCaches.get(1));
+            long runtime = stopWatch.getTime();
+
+            // evaluate
+            // compare results from limes to test set
+            evaluate(predictions, testSet, runtime, mlResults.getLinkSpecification(), resultsFileTest, csvWriterTest);
+            // compare mapping from MLResults with the training set
+            evaluate(mlResults.getMapping(), trainingSet, 0, mlResults.getLinkSpecification(), resultsFileTraining,
+                    csvWriterTrain);
         }
     }
 
@@ -178,10 +258,8 @@ public class Semantics {
         return result;
     }
 
-    static void executeLinkSpecs(LinkSpecification linkSpecification, ACache sourceCache, ACache targetCache,
-            AMapping referenceSet) {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+    static AMapping executeLinkSpecs(LinkSpecification linkSpecification, ACache sourceCache, ACache targetCache) {
+
         // AMapping kbMap;
         Rewriter rw = RewriterFactory.getDefaultRewriter();
         LinkSpecification rwLs = rw.rewrite(linkSpecification);
@@ -192,13 +270,12 @@ public class Semantics {
         assert engine != null;
         AMapping resultMap = engine.execute(rwLs, planner);
         // kbMap = resultMap.getSubMap(linkSpecification.getThreshold());
-        long runtime = stopWatch.getTime();
-
-        evaluate(resultMap, referenceSet, runtime, linkSpecification);
+        return resultMap;
 
     }
 
-    public static void evaluate(AMapping predictions, AMapping referenceSet, long runtime, LinkSpecification linkSpecification) {
+    public static void evaluate(AMapping predictions, AMapping referenceSet, long runtime,
+            LinkSpecification linkSpecification, String file, CSVWriter writer) {
         QualitativeMeasuresEvaluator evaluator = new QualitativeMeasuresEvaluator();
         Set<EvaluatorType> evaluationMeasures = new LinkedHashSet<EvaluatorType>();
         evaluationMeasures.add(EvaluatorType.PRECISION);
@@ -208,11 +285,12 @@ public class Semantics {
         Map<EvaluatorType, Double> evaluations = evaluator.evaluate(predictions, new GoldStandard(referenceSet),
                 evaluationMeasures);
 
-        writeResults(evaluations, runtime, predictions.getNumberofMappings(), referenceSet.getNumberofMappings(),linkSpecification);
+        writeResults(evaluations, runtime, predictions.getNumberofMappings(), referenceSet.getNumberofMappings(),
+                linkSpecification, file, writer);
     }
 
     public static void writeResults(Map<EvaluatorType, Double> evaluations, long runtime, int predictions,
-            int reference, LinkSpecification linkSpecification) {
+            int reference, LinkSpecification linkSpecification, String file, CSVWriter writer) {
 
         String[] values = new String[8];
         values[0] = String.valueOf(reference);
@@ -226,19 +304,19 @@ public class Semantics {
         }
         values[index] = String.valueOf(runtime);
         index++;
-        values[index] = linkSpecification.getFullExpression()+">="+linkSpecification.getThreshold();
+        values[index] = linkSpecification.getFullExpression() + ">=" + linkSpecification.getThreshold();
 
         try {
-            csvWriter = new CSVWriter(new FileWriter(resultsFile, true));
+            writer = new CSVWriter(new FileWriter(file, true));
         } catch (IOException e) {
             logger.error("Can't create csv writer");
             e.printStackTrace();
             throw new RuntimeException();
         }
-        csvWriter.writeNext(values, false);
+        writer.writeNext(values, false);
 
         try {
-            csvWriter.close();
+            writer.close();
         } catch (IOException e) {
             logger.error("Couldn't close csv writer");
             e.printStackTrace();
@@ -283,7 +361,6 @@ public class Semantics {
 
         return sample;
     }
-
 
     private static List<AMapping> generateEvaluationSets(AMapping fullReferenceMapping) {
         AMapping localRef = fullReferenceMapping.getSubMap(0.0d);
