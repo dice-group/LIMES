10a500ee596e969a9dab4445ffc51fa7303df7b8
Kleanthi Georgala <kleanthi@ainote6.informatik.uni-leipzig.de>
2019-09-16 16:39:11 +0100

    Adding to the temporal relation mapper the ability to parse different time stamp formats

---
+++ limes-core/src/test/java/org/aksw/limes/core/measures/mapper/temporal/allenAlgebra/atomic/AAtomicAllenAlgebraMapperTest.java
@@ -0,0 +1,60 @@
+package org.aksw.limes.core.measures.mapper.temporal.allenAlgebra.atomic;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class AAtomicAllenAlgebraMapperTest {
+
+    @Test
+    public void test() {
+
+       
+        
+        String[] values = new String[] { 
+                "2015-05-20T08:21:04.123Z",
+                "2015-05-20T08:21:04.123+02:00",
+                
+                "2015-05-20T08:21:04.123",
+                
+                "2015-05-20T08:21:04Z",
+                "2015-05-20T08:21:04+02:00",
+                
+                "2015-05-20T08:21:04",
+                
+                "2015-05-20T08:21Z",
+                "2015-05-20T08:21+02:00",
+                
+                "2015-05-20T08:21",
+                "2015-05-20T08:21:00",
+                
+                //"2015-05-20",
+                
+                //"2015-05",
+                
+                //"2015",
+                
+        };
+
+        ArrayList<Long> numbers = new ArrayList<Long>();
+        
+        for(String value: values){
+            long epoch = AAtomicAllenAlgebraMapper.getEpoch(value);
+            System.out.println(epoch);
+            numbers.add(epoch);
+            System.out.println("-----------------------");
+        }
+        //7200000
+        assertTrue(numbers.get(0)-numbers.get(1) == 7200000);
+        
+        assertTrue(numbers.get(3)-numbers.get(4) == 7200000);
+        
+        assertTrue(numbers.get(6)-numbers.get(7) == 7200000);
+        
+        assertTrue(numbers.get(8).equals(numbers.get(9)));
+
+    }
+
+}
