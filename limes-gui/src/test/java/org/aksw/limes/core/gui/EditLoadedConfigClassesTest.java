78939a42749b24a6092c9a8f735db4f4546abebf
Kevin Dreßler <kvndrsslr@gmail.com>
2019-10-07 09:11:39 +0200

    disable unreliable GUI tests

--- limes-gui/src/test/java/org/aksw/limes/core/gui/EditLoadedConfigClassesTest.java
+++ limes-gui/src/test/java/org/aksw/limes/core/gui/EditLoadedConfigClassesTest.java
@@ -1,108 +1,108 @@
-package org.aksw.limes.core.gui;
-
-import static org.junit.Assert.assertEquals;
-import static org.testfx.api.FxAssert.verifyThat;
-
-import java.io.File;
-import java.util.Locale;
-
-import org.aksw.limes.core.gui.controller.MainController;
-import org.aksw.limes.core.gui.model.Config;
-import org.aksw.limes.core.gui.model.Endpoint;
-import org.aksw.limes.core.gui.util.CustomGuiTest;
-import org.aksw.limes.core.gui.util.ProjectPropertiesGetter;
-import org.aksw.limes.core.gui.view.MainView;
-import org.aksw.limes.core.io.config.KBInfo;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testfx.api.FxRobot;
-import org.testfx.framework.junit.ApplicationTest;
-import org.testfx.matcher.base.NodeMatchers;
-
-import javafx.application.Platform;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-
-public class EditLoadedConfigClassesTest extends ApplicationTest {
-
-	private static final Logger logger = LoggerFactory.getLogger(EditLoadedConfigClassesTest.class);
-	MainView mainView;
-	MainController mainController;
-	private String resourcesPath;
-
-	@Override
-	public void start(Stage stage) throws Exception {
-		Locale.setDefault(new Locale("en", "US"));
-		this.mainView = new MainView(stage);
-		this.mainController = new MainController(this.mainView);
-		this.mainView.setController(this.mainController);
-		this.resourcesPath = ProjectPropertiesGetter.getProperty(
-				Thread.currentThread().getContextClassLoader().getResource("project.properties").getPath(),
-				"limes-core.resources");
-	}
-
-	@Before
-	public void loadConfig() {
-		this.mainController.loadConfig(
-				new File(Thread.currentThread().getContextClassLoader().getResource("gui/testConfig.xml").getFile()));
-		final Config c = this.mainController.getCurrentConfig();
-		final KBInfo sinfo = new KBInfo();
-		final KBInfo tinfo = new KBInfo();
-		final String restaurantsEndpoint = this.resourcesPath + "datasets/Restaurants/restaurant1.nt";
-		final String personsEndpoint = this.resourcesPath + "datasets/Persons2/person21.nt";
-		sinfo.setEndpoint(restaurantsEndpoint);
-		sinfo.setId("Restaurants");
-		final Endpoint sendpoint = new Endpoint(sinfo, c);
-		sendpoint.update();
-		c.setSourceEndpoint(sendpoint);
-
-		tinfo.setEndpoint(personsEndpoint);
-		tinfo.setId("Restaurants");
-		final Endpoint tendpoint = new Endpoint(tinfo, c);
-		tendpoint.update();
-		c.setTargetEndpoint(tendpoint);
-	}
-
-	@BeforeClass
-	public static void setup() {
-		System.setProperty("testfx.robot", "glass");
-		System.setProperty("testfx.headless", "true");
-		System.setProperty("prism.order", "sw");
-		System.setProperty("prism.text", "t2k");
-		System.setProperty("java.awt.headless", "true");
-	}
-
-	@Test
-	public void testEditClassMatching() {
-		logger.info("Clicking on Configuration");
-		this.clickOn("Configuration");
-		logger.info("Clicking on Edit");
-		this.clickOn("Edit");
-		logger.info("Clicking on Classes");
-		this.clickOn("Edit Classes");
-
-		logger.info("Waiting for classes to be visible");
-		CustomGuiTest.waitUntilLoadingWindowIsClosed("Get classes", 500);
-		CustomGuiTest.waitUntilNodeIsVisible("Restaurant", 150);
-		CustomGuiTest.waitUntilNodeIsVisible("Person", 15);
-		verifyThat("Restaurant", NodeMatchers.isVisible());
-		verifyThat("Person", NodeMatchers.isVisible());
-	}
-
-	@AfterClass
-	public static void cleanup() {
-		final FxRobot rob = new FxRobot();
-		for (final Window w : rob.listWindows()) {
-			final int currentsize = rob.listWindows().size();
-			System.out.println(((Stage) w).getTitle());
-			// Avoid not on fx application thread error
-			Platform.runLater(() -> ((Stage) w).close());
-			CustomGuiTest.waitUntilWindowIsClosed(currentsize - 1, 200);
-		}
-		assertEquals(0, rob.listWindows().size());
-	}
-}
+//package org.aksw.limes.core.gui;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.testfx.api.FxAssert.verifyThat;
+//
+//import java.io.File;
+//import java.util.Locale;
+//
+//import org.aksw.limes.core.gui.controller.MainController;
+//import org.aksw.limes.core.gui.model.Config;
+//import org.aksw.limes.core.gui.model.Endpoint;
+//import org.aksw.limes.core.gui.util.CustomGuiTest;
+//import org.aksw.limes.core.gui.util.ProjectPropertiesGetter;
+//import org.aksw.limes.core.gui.view.MainView;
+//import org.aksw.limes.core.io.config.KBInfo;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.testfx.api.FxRobot;
+//import org.testfx.framework.junit.ApplicationTest;
+//import org.testfx.matcher.base.NodeMatchers;
+//
+//import javafx.application.Platform;
+//import javafx.stage.Stage;
+//import javafx.stage.Window;
+//
+//public class EditLoadedConfigClassesTest extends ApplicationTest {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(EditLoadedConfigClassesTest.class);
+//	MainView mainView;
+//	MainController mainController;
+//	private String resourcesPath;
+//
+//	@Override
+//	public void start(Stage stage) throws Exception {
+//		Locale.setDefault(new Locale("en", "US"));
+//		this.mainView = new MainView(stage);
+//		this.mainController = new MainController(this.mainView);
+//		this.mainView.setController(this.mainController);
+//		this.resourcesPath = ProjectPropertiesGetter.getProperty(
+//				Thread.currentThread().getContextClassLoader().getResource("project.properties").getPath(),
+//				"limes-core.resources");
+//	}
+//
+//	@Before
+//	public void loadConfig() {
+//		this.mainController.loadConfig(
+//				new File(Thread.currentThread().getContextClassLoader().getResource("gui/testConfig.xml").getFile()));
+//		final Config c = this.mainController.getCurrentConfig();
+//		final KBInfo sinfo = new KBInfo();
+//		final KBInfo tinfo = new KBInfo();
+//		final String restaurantsEndpoint = this.resourcesPath + "datasets/Restaurants/restaurant1.nt";
+//		final String personsEndpoint = this.resourcesPath + "datasets/Persons2/person21.nt";
+//		sinfo.setEndpoint(restaurantsEndpoint);
+//		sinfo.setId("Restaurants");
+//		final Endpoint sendpoint = new Endpoint(sinfo, c);
+//		sendpoint.update();
+//		c.setSourceEndpoint(sendpoint);
+//
+//		tinfo.setEndpoint(personsEndpoint);
+//		tinfo.setId("Restaurants");
+//		final Endpoint tendpoint = new Endpoint(tinfo, c);
+//		tendpoint.update();
+//		c.setTargetEndpoint(tendpoint);
+//	}
+//
+//	@BeforeClass
+//	public static void setup() {
+//		System.setProperty("testfx.robot", "glass");
+//		System.setProperty("testfx.headless", "true");
+//		System.setProperty("prism.order", "sw");
+//		System.setProperty("prism.text", "t2k");
+//		System.setProperty("java.awt.headless", "true");
+//	}
+//
+//	@Test
+//	public void testEditClassMatching() {
+//		logger.info("Clicking on Configuration");
+//		this.clickOn("Configuration");
+//		logger.info("Clicking on Edit");
+//		this.clickOn("Edit");
+//		logger.info("Clicking on Classes");
+//		this.clickOn("Edit Classes");
+//
+//		logger.info("Waiting for classes to be visible");
+//		CustomGuiTest.waitUntilLoadingWindowIsClosed("Get classes", 500);
+//		CustomGuiTest.waitUntilNodeIsVisible("Restaurant", 150);
+//		CustomGuiTest.waitUntilNodeIsVisible("Person", 15);
+//		verifyThat("Restaurant", NodeMatchers.isVisible());
+//		verifyThat("Person", NodeMatchers.isVisible());
+//	}
+//
+//	@AfterClass
+//	public static void cleanup() {
+//		final FxRobot rob = new FxRobot();
+//		for (final Window w : rob.listWindows()) {
+//			final int currentsize = rob.listWindows().size();
+//			System.out.println(((Stage) w).getTitle());
+//			// Avoid not on fx application thread error
+//			Platform.runLater(() -> ((Stage) w).close());
+//			CustomGuiTest.waitUntilWindowIsClosed(currentsize - 1, 200);
+//		}
+//		assertEquals(0, rob.listWindows().size());
+//	}
+//}
--- limes-gui/src/test/java/org/aksw/limes/core/gui/util/CustomGuiTest.java
+++ limes-gui/src/test/java/org/aksw/limes/core/gui/util/CustomGuiTest.java
@@ -1,169 +1,169 @@
-package org.aksw.limes.core.gui.util;
-
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testfx.api.FxRobot;
-
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-
-public class CustomGuiTest {
-
-	private static final Logger logger = LoggerFactory.getLogger(CustomGuiTest.class);
-
-	public static boolean waitUntilNodeIsNotNull(String nodeId, int timeout) {
-		Node node = new FxRobot().lookup(nodeId).query();
-		while (node == null && timeout != 0) {
-			try {
-				node = new FxRobot().lookup(nodeId).query();
-				timeout--;
-				logger.info("Timeoutremaining for " + nodeId + " : " + timeout);
-				Thread.sleep(1000);
-			} catch (final InterruptedException e) {
-				logger.error("Interrupted while waiting for Node " + nodeId + " to be not null!");
-				e.printStackTrace();
-			}
-		}
-		if (node != null) {
-			logger.info(nodeId + " not null");
-			return true;
-		}
-		logger.error("Maximum timeout reached, while waiting for Node " + nodeId + " to be not null!");
-		return false;
-	}
-
-	/**
-	 * Waits until windows is closed or timeout in seconds is reached
-	 * 
-	 * @param name
-	 *            of window
-	 * @param timeout
-	 */
-	public static void waitUntilLoadingWindowIsClosed(String windowname, int timeout) {
-		final FxRobot rob = new FxRobot();
-		int sec = 0;
-		logger.info("Wait until " + windowname + " is closed");
-		logger.info("Currently open windows: ");
-		for (final Window w : rob.listWindows()) {
-			logger.info(((Stage) w).getTitle());
-		}
-		do {
-			boolean closed = true;
-			for (final Window w : rob.listWindows()) {
-				if (((Stage) w).getTitle().trim().equals(windowname.trim())) {
-					closed = false;
-				}
-			}
-			if (closed) {
-				break;
-			}
-			rob.sleep(1000);
-			sec++;
-			if (sec % 100 == 0) {
-				logger.info("Waited: " + sec + " seconds");
-			}
-			// avoid infinite loop
-			if (sec > timeout) {
-				break;
-			}
-		} while (true);
-	}
-
-	/**
-	 * Waits until number of windows is reduced to n or timeout in seconds is
-	 * reached
-	 * 
-	 * @param n
-	 *            number of desired windows
-	 * @param timeout
-	 */
-	public static void waitUntilWindowIsClosed(int n, int timeout) {
-		final FxRobot rob = new FxRobot();
-		int sec = 0;
-		logger.info("Wait until " + n + " windows are left open");
-		logger.info("Currently open windows: ");
-		for (final Window w : rob.listWindows()) {
-			logger.info(((Stage) w).getTitle());
-		}
-		do {
-			rob.sleep(1000);
-			sec++;
-			if (sec % 100 == 0) {
-				logger.info("Waited: " + sec + " seconds");
-			}
-			// avoid infinite loop
-			if (sec > timeout) {
-				break;
-			}
-		} while (rob.listWindows().size() > n);
-	}
-
-	/**
-	 * Uses FxRobot from TestFX to lookup the node
-	 *
-	 * @param nodeId
-	 *            ID of the node
-	 * @param timeout
-	 *            in seconds
-	 */
-	public static void waitUntilNodeIsVisible(String nodeId, int timeout) {
-		Node node = new FxRobot().lookup(nodeId).query();
-		boolean found;
-		if (node == null) {
-			found = waitUntilNodeIsNotNull(nodeId, timeout);
-		} else {
-			found = true;
-		}
-		if (!found) {
-			logger.error("Maximum timeout reached, while waiting for Node " + nodeId + " to be visible!");
-			return;
-		}
-		node = new FxRobot().lookup(nodeId).query();
-		while (!node.isVisible() && timeout != 0) {
-			try {
-				timeout--;
-				if (timeout % 10 == 0) {
-					logger.info("Timeoutremaining for " + nodeId + " : " + timeout);
-				}
-				Thread.sleep(1000);
-			} catch (final InterruptedException e) {
-				logger.error("Interrupted while waiting for Node " + nodeId + " to be visible!");
-				e.printStackTrace();
-			}
-		}
-		if (node.isVisible()) {
-			logger.info(nodeId + " is visible");
-		} else {
-			logger.info(nodeId + " is NOT visible");
-		}
-
-	}
-
-	public static TableRow<?> getFirstRowOfTableView(String tableSelector) {
-		final TableView<?> tableView = new FxRobot().lookup(tableSelector).query();
-
-		List<Node> current = tableView.getChildrenUnmodifiable();
-		while (current.size() == 1) {
-			current = ((Parent) current.get(0)).getChildrenUnmodifiable();
-		}
-
-		current = ((Parent) current.get(1)).getChildrenUnmodifiable();
-		while (!(current.get(0) instanceof TableRow)) {
-			current = ((Parent) current.get(0)).getChildrenUnmodifiable();
-		}
-
-		final Node node = current.get(0);
-		if (node instanceof TableRow) {
-			return (TableRow<?>) node;
-		} else {
-			throw new RuntimeException("Expected Group with only TableRows as children");
-		}
-	}
-
-}
+//package org.aksw.limes.core.gui.util;
+//
+//import java.util.List;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.testfx.api.FxRobot;
+//
+//import javafx.scene.Node;
+//import javafx.scene.Parent;
+//import javafx.scene.control.TableRow;
+//import javafx.scene.control.TableView;
+//import javafx.stage.Stage;
+//import javafx.stage.Window;
+//
+//public class CustomGuiTest {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(CustomGuiTest.class);
+//
+//	public static boolean waitUntilNodeIsNotNull(String nodeId, int timeout) {
+//		Node node = new FxRobot().lookup(nodeId).query();
+//		while (node == null && timeout != 0) {
+//			try {
+//				node = new FxRobot().lookup(nodeId).query();
+//				timeout--;
+//				logger.info("Timeoutremaining for " + nodeId + " : " + timeout);
+//				Thread.sleep(1000);
+//			} catch (final InterruptedException e) {
+//				logger.error("Interrupted while waiting for Node " + nodeId + " to be not null!");
+//				e.printStackTrace();
+//			}
+//		}
+//		if (node != null) {
+//			logger.info(nodeId + " not null");
+//			return true;
+//		}
+//		logger.error("Maximum timeout reached, while waiting for Node " + nodeId + " to be not null!");
+//		return false;
+//	}
+//
+//	/**
+//	 * Waits until windows is closed or timeout in seconds is reached
+//	 *
+//	 * @param name
+//	 *            of window
+//	 * @param timeout
+//	 */
+//	public static void waitUntilLoadingWindowIsClosed(String windowname, int timeout) {
+//		final FxRobot rob = new FxRobot();
+//		int sec = 0;
+//		logger.info("Wait until " + windowname + " is closed");
+//		logger.info("Currently open windows: ");
+//		for (final Window w : rob.listWindows()) {
+//			logger.info(((Stage) w).getTitle());
+//		}
+//		do {
+//			boolean closed = true;
+//			for (final Window w : rob.listWindows()) {
+//				if (((Stage) w).getTitle().trim().equals(windowname.trim())) {
+//					closed = false;
+//				}
+//			}
+//			if (closed) {
+//				break;
+//			}
+//			rob.sleep(1000);
+//			sec++;
+//			if (sec % 100 == 0) {
+//				logger.info("Waited: " + sec + " seconds");
+//			}
+//			// avoid infinite loop
+//			if (sec > timeout) {
+//				break;
+//			}
+//		} while (true);
+//	}
+//
+//	/**
+//	 * Waits until number of windows is reduced to n or timeout in seconds is
+//	 * reached
+//	 *
+//	 * @param n
+//	 *            number of desired windows
+//	 * @param timeout
+//	 */
+//	public static void waitUntilWindowIsClosed(int n, int timeout) {
+//		final FxRobot rob = new FxRobot();
+//		int sec = 0;
+//		logger.info("Wait until " + n + " windows are left open");
+//		logger.info("Currently open windows: ");
+//		for (final Window w : rob.listWindows()) {
+//			logger.info(((Stage) w).getTitle());
+//		}
+//		do {
+//			rob.sleep(1000);
+//			sec++;
+//			if (sec % 100 == 0) {
+//				logger.info("Waited: " + sec + " seconds");
+//			}
+//			// avoid infinite loop
+//			if (sec > timeout) {
+//				break;
+//			}
+//		} while (rob.listWindows().size() > n);
+//	}
+//
+//	/**
+//	 * Uses FxRobot from TestFX to lookup the node
+//	 *
+//	 * @param nodeId
+//	 *            ID of the node
+//	 * @param timeout
+//	 *            in seconds
+//	 */
+//	public static void waitUntilNodeIsVisible(String nodeId, int timeout) {
+//		Node node = new FxRobot().lookup(nodeId).query();
+//		boolean found;
+//		if (node == null) {
+//			found = waitUntilNodeIsNotNull(nodeId, timeout);
+//		} else {
+//			found = true;
+//		}
+//		if (!found) {
+//			logger.error("Maximum timeout reached, while waiting for Node " + nodeId + " to be visible!");
+//			return;
+//		}
+//		node = new FxRobot().lookup(nodeId).query();
+//		while (!node.isVisible() && timeout != 0) {
+//			try {
+//				timeout--;
+//				if (timeout % 10 == 0) {
+//					logger.info("Timeoutremaining for " + nodeId + " : " + timeout);
+//				}
+//				Thread.sleep(1000);
+//			} catch (final InterruptedException e) {
+//				logger.error("Interrupted while waiting for Node " + nodeId + " to be visible!");
+//				e.printStackTrace();
+//			}
+//		}
+//		if (node.isVisible()) {
+//			logger.info(nodeId + " is visible");
+//		} else {
+//			logger.info(nodeId + " is NOT visible");
+//		}
+//
+//	}
+//
+//	public static TableRow<?> getFirstRowOfTableView(String tableSelector) {
+//		final TableView<?> tableView = new FxRobot().lookup(tableSelector).query();
+//
+//		List<Node> current = tableView.getChildrenUnmodifiable();
+//		while (current.size() == 1) {
+//			current = ((Parent) current.get(0)).getChildrenUnmodifiable();
+//		}
+//
+//		current = ((Parent) current.get(1)).getChildrenUnmodifiable();
+//		while (!(current.get(0) instanceof TableRow)) {
+//			current = ((Parent) current.get(0)).getChildrenUnmodifiable();
+//		}
+//
+//		final Node node = current.get(0);
+//		if (node instanceof TableRow) {
+//			return (TableRow<?>) node;
+//		} else {
+//			throw new RuntimeException("Expected Group with only TableRows as children");
+//		}
+//	}
+//
+//}
