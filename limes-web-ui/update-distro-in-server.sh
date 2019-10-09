4b2f684f6ba8e88fe62104a80eb73ebf1d0a8220
Kevin Dreßler <kvndrsslr@gmail.com>
2019-10-09 15:29:04 +0200

    fix manual (hopefully), add bash script to update vuejs web ui

--- limes-web-ui/update-distro-in-server.sh
+++ limes-web-ui/update-distro-in-server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+npm install
 npm run build
-cp -rf "${DIR}/dist" "${DIR}/../limes-core/src/main/resources/web-ui"
+cp -rf "${DIR}/dist/" "${DIR}/../limes-core/src/main/resources/web-ui"
