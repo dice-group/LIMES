1d833ca15ddbaf17a16fa79adfa9576225a6fef3
Kevin Dreßler <kvndrsslr@gmail.com>
2019-10-10 12:23:34 +0200

    updated script, removed unnecessary files from web ui

--- limes-web-ui/update-distro-in-server.sh
+++ limes-web-ui/update-distro-in-server.sh
@@ -2,4 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 npm install
 npm run build
+rm -rf "${DIR}/../limes-core/src/main/resources/web-ui"
+git rm --cached -r "${DIR}/../limes-core/src/main/resources/web-ui"
 cp -rf "${DIR}/dist/" "${DIR}/../limes-core/src/main/resources/web-ui"
+git add "${DIR}/../limes-core/src/main/resources/web-ui"
