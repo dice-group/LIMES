c2030e4f6104546b64e4f62670d26bdbf5e6b532
Kevin Dreßler <kvndrsslr@gmail.com>
2019-10-10 15:36:08 +0200

    fixed web ui generation

--- limes-web-ui/update-distro-in-server.sh
+++ limes-web-ui/update-distro-in-server.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "${DIR}";
 npm install
 npm run build
 rm -rf "${DIR}/../limes-core/src/main/resources/web-ui"
 git rm --cached -r "${DIR}/../limes-core/src/main/resources/web-ui"
 cp -rf "${DIR}/dist/" "${DIR}/../limes-core/src/main/resources/web-ui"
 git add "${DIR}/../limes-core/src/main/resources/web-ui"
+popd;
